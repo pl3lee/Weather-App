8e57b80f3d27ae5aebde6ffd9b882757ac79c900
pl3lee <billylee123@gmail.com>
2022-05-29 15:49:00 -0400

    created general layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 import "./style.css";
 
-let hello = 1;
-let test = "hello";
-let test1 = "hello";
+class Location {
+    constructor(name) {
+        this.name = name;
+        this.#fetchData();
+    }
+    async #fetchData() {
+        const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${this.name}&units=metric&APPID=21aa6ecbbe7f60001df5a96e864081a4`
+        );
+        const data = await response.json();
+        console.log(data);
+        this.weather = data.weather[0].main;
+        this.weatherDescription = data.weather[0].description;
+        this.temp = data.main.temp;
+        this.feelsLike = data.main.feels_like;
+        this.pressure = data.main.pressure;
+        this.humidity = data.main.humidity;
+        this.minTemp = data.main.temp_min;
+        this.maxTemp = data.main.temp_max;
+        this.country = data.sys.country;
+    }
+}
+
+let test = new Location("London");
+console.log(test);
